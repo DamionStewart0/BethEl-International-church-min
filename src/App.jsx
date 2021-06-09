89042c82569b9928d71becd58f3119f13c2a90b1
Developer genius <developer_genius@protonmail.com>
2021-06-09 11:03:20 -0400

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import {Route, switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route exact path='/about-us'>
+          <About />
+        </Route>
+
+        <Route exact path='/ministries'>
+          <Ministries />
+        </Route>
+
+        <Route exact path='/biography'>
+          <Biography />
+        </Route>
+
+        <Route exact path='/hebrew-in-you'>
+          <Hebrew />
+        </Route>
+        <Route exact path='/store'>
+          <Store />
+        </Route>
+        
+
+
+
+
+
+      </Switch>
       
     </div>
   );
-}
+};
 
 export default App;
