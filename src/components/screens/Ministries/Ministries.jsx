ea2cc7df6c7dbd3d178a9ef2e99f16bfad556166
Developer genius <developer_genius@protonmail.com>
2021-07-01 13:04:32 -0400

    added a shoecase to ministries laying out the content

--- src/components/screens/Ministries/Ministries.jsx
+++ src/components/screens/Ministries/Ministries.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import Layout from '../../shared/Layout/Layout'
+import React from "react";
+import Layout from "../../shared/Layout/Layout";
+import "./Ministries.css";
 
 const Ministries = () => {
-    return (
-        <Layout>
+  return (
+    <Layout>
+    
+      <div className="congregation">
+        <img
+          src="https://static.wixstatic.com/media/cef3ab_7a89607fc5c042da9dcf930148f85237~mv2_d_4032_1960_s_2.jpg/v1/fill/w_397,h_193,q_90/cef3ab_7a89607fc5c042da9dcf930148f85237~mv2_d_4032_1960_s_2.jpg"
+          alt="chruch"
+        ></img>
+        <h1>Ministries</h1>
+      </div>
+      <section className="ministries">
+        <img src="https://images.unsplash.com/photo-1612375411759-b35b2f080448?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG9yYWh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='torah'></img>
+        {/* <h3>Men Of BethEl (M.O.B)</h3> */}
+        <p>
+          Building the Body of the Mashiach through ministry to Men” Our mission
+          is to present every man with a credible offer of the Gospel, encourage
+          them to pursue YHWH, live a Biblical life of moral integrity and equip
+          them for spiritual service in the home, church, workplace
+          <p>
+            Our goals are two-fold: A. To identify and equip at least one man in
+            every willing BEICM Ministry who wants to build a disciple-making
+            ministry to men through weekly meetings. B. And that there is a
+            men’s ministry group in every BEICM affiliate Ministry/ Church E
+          </p>
+        </p>
+      </section>
 
-        <div>
-            <h1>This is Ministries</h1>
-        </div>
-        </Layout>
-    )
-}
+      <section className="ministries">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla velit minus magnam veritatis aliquid ipsam, molestiae maiores voluptate commodi itaque nam natus ipsum saepe consectetur deleniti harum delectus, quis odio!</p>
+          {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto maxime, ipsam saepe odit dicta quibusdam magni dolore itaque sequi soluta, veritatis nesciunt! Iusto eaque corrupti, incidunt similique recusandae tempore fugit.
+          Id non veniam, facilis numquam quas quam reprehenderit blanditiis, iusto, tenetur incidunt corporis dignissimos soluta ipsam esse modi. Illo eveniet ut maiores</p> */}
 
-export default Ministries
+          <img src="https://images.unsplash.com/photo-1612375411759-b35b2f080448?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dG9yYWh8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt='torah'></img>
+
+      </section>
+
+
+
+
+
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+      <section className="ministries"></section>
+    </Layout>
+  );
+};
+
+export default Ministries;
